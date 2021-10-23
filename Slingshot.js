3fab4ea034ea74ae7b0036a442e6f2d95dfd133b
KapishS <84273328+itsKapish@users.noreply.github.com>
2021-10-23 09:35:30 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,27 @@
+ class Slingshot{
+
+ constructor(bodyA, bodyB){
+     
+    var options = {
+     bodyA: bodyA, bodyB: bodyB, length: 100, stiffness: 0.04
+    }
+ 
+   this.sling = Constraint.create(options);
+   World.add(world, this.sling);
+
+ }
+
+ display(){
+
+    var pointA = this.sling.bodyA.position
+    var pointB = this.sling.bodyB.position
+
+ push();
+
+ strokeWeight(5);
+ line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+ pop(); 
+
+ }
+ }
